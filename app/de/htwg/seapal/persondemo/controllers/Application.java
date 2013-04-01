4c937324d70bdbeb3e4862fd0c1b8cabb8d87077
markoboger <marko.boger@gmail.com>
2013-04-01 16:51:29 +0200

    tests run, package rename seams complete, version 1.1

--- app/de/htwg/seapal/persondemo/controllers/Application.java
+++ app/de/htwg/seapal/persondemo/controllers/Application.java
@@ -3,13 +3,12 @@ package de.htwg.seapal.persondemo.controllers;
 import play.*;
 import play.mvc.*;
 
-import views.html.*;
+import de.htwg.seapal.persondemo.views.html.*;
 
 public class Application extends Controller {
   
     public static Result index() {
-        //return ok(index.render("Your new application is ready."));
-    	return ok();
+        return ok(index.render("Your new application is ready."));
     }
   
 }
